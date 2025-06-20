3abed86029a3fb30699093b681424c38fdd72fbc
Aidan Burrowes <84931497+FluffyNova@users.noreply.github.com>
2025-06-20 10:32:20 -0700

    Making sure static is tracked

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ AudioSet by Google at https://research.google.com/audioset/download.html
 '''
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 CORS(app)
 
 # def main():
