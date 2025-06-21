2205683fee2d84a5884f2051fae2fb263ea196d5
Aidan Burrowes <84931497+FluffyNova@users.noreply.github.com>
2025-06-20 19:17:55 -0700

    Configuring for frontend

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
-import os
+from flask import Flask, request, jsonify, send_file, render_template
+import requests
+from songs import getSong, getAllSongs
+from artists import getAllArtists
+from flask_cors import CORS
 
 '''
 RVC GUI by Tiger14n at https://github.com/Tiger14n/RVC-GUI
@@ -7,11 +10,79 @@ The Weeknd (RVC) 1000 Epochs by clubbedsam#4419 at https://docs.google.com/sprea
 AudioSet by Google at https://research.google.com/audioset/download.html
 '''
 
-app = Flask(__name__, static_folder="static", template_folder="templates")
+app = Flask(__name__, static_folder='static', template_folder='templates')
+CORS(app)
 
+BACKEND_URL = "https://huggingface.co/spaces/FluffyNova/Melody-Mimicry"  # Space inference endpoint
+
+# def main():
+    # createAIVocals(
+    #     'https://www.youtube.com/watch?v=adLGHcj_fmA&pp=ygUTbGVhdmUgdGhlIGRvb3Igb3Blbg%3D%3D', 
+    #     'Leave the Door Open', 
+    #     'lil_uzi_vert_37K_1000'
+    # )
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+@app.route("/get_all_artists", methods=['GET'])
+def get_all_artists():
+    all_artists = getAllArtists()
+    print(all_artists)
+
+    all_artists_arr = []
+    for artist in all_artists:
+        all_artists_arr.append(artist[0].replace('\'', ''))
+
+    return jsonify(all_artists_arr)
+
+@app.route("/get_all_songs", methods=['GET'])
+def get_all_songs():
+    all_songs = getAllSongs()
+    print(all_songs)
+
+    all_songs_arr = []
+    for song in all_songs:
+        all_songs_arr.append(song[0].replace('\'', ''))
+
+    return jsonify(all_songs_arr)
+
+@app.route("/get_song", methods=['POST'])
+def get_song():
+    artist_and_song_request = request.data.decode('UTF8').replace("[", "").replace("]", "").replace("\"","")
+    print(artist_and_song_request)
+    artist_and_song_data = artist_and_song_request.split(',')
+
+    artist_name = artist_and_song_data[0]
+    song_name = artist_and_song_data[1]
+
+    if artist_name == None or song_name == None:
+        return jsonify("Error: Artist or song not found")
+
+    print("Artist name:", artist_name)
+    print("Song name:", song_name)
+
+    try:
+        data = request.get_json()
+        link = data["link"]
+        song = data["song"]
+        model = data["model"]
+
+        response = requests.post(
+            BACKEND_URL,
+            json={"data": [link, song, model]}
+        )
+        song_file = response.json()
+
+    except Exception as e:
+        return jsonify(f"Error: {str(e)}")
+
+    if song_file == None or not song_file.endswith('.wav') and not song_file.endswith('.mp3'):
+        return jsonify(f'{song_file}')
+    # song_file = 'static/output/lil_uzi_vert_37K_1000_Leave the Door Open_RVC.mp3'
+    
+    return jsonify(song_file)
+
+if __name__ == '__main__':
+    # main()
+    app.run(host='127.0.0.1', port=8080)
